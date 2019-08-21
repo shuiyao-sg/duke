df4751073f4c049a490650b63efb033f65efbab9
shuiyao-sg <shuiyao@u.nus.edu>
2019-08-21 11:02:57 +0800

    Implement Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello! I'm Duke\n" +
+                "What can I do for you?");
+
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (!input.equals("bye")) {
+                System.out.println(input);
+            } else {
+                System.out.println("Bye. Hope to see you again soon!");
+                sc.close();
+                break;
+            }
+        }
+
+
     }
 }
