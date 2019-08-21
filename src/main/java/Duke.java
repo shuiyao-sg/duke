016cbdf33b871d122b8b8b38e8073d23276309e0
shuiyao-sg <shuiyao@u.nus.edu>
2019-08-21 11:59:51 +0800

    Implement Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,20 +11,51 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+        String indentByFour = " " + " " + " " + " ";
+        String indentByOne = " ";
+        String horizontalLine = indentByFour + "____________________________________________________________";
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello! I'm Duke\n" +
-                "What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.print(indentByFour + indentByOne);
+        System.out.println("Hello! I'm Duke");
+        System.out.print(indentByFour + indentByOne);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println();
+
+        List<String> list = new ArrayList<>();
 
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
-            if (!input.equals("bye")) {
-                System.out.println(input);
-            } else {
+
+            if (input.equals("bye")) {
+                System.out.println(horizontalLine);
+                System.out.print(indentByFour + indentByOne);
                 System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(horizontalLine);
                 sc.close();
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(horizontalLine);
+                for (int i = 0; i < list.size(); i++) {
+                    int index = i + 1;
+                    System.out.print(indentByFour + indentByOne);
+                    System.out.println(index + "." + " " + list.get(i));
+                }
+                System.out.println(horizontalLine);
+                System.out.println();
+            } else {
+                list.add(input);
+                System.out.println(horizontalLine);
+                System.out.print(indentByFour + indentByOne);
+                System.out.println("Added:" + " " + input);
+                System.out.println(horizontalLine);
+                System.out.println();
             }
+
+
         }
 
 
