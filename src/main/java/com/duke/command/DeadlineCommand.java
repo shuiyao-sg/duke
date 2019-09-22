3e21d3179d4c3eef062c0c6cd0cc8293fc6437da
shuiyao-sg <shuiyao@u.nus.edu>
2019-09-22 16:54:10 +0800

    Add AddCommand.java. ToDoCommand, DeadlineCommand, EventCommand extend AddCommand

--- src/main/java/com/duke/command/DeadlineCommand.java
+++ src/main/java/com/duke/command/DeadlineCommand.java
@@ -1,18 +1,12 @@
 package com.duke.command;
 
-import com.duke.Storage;
-import com.duke.task.Deadline;
 import com.duke.TaskList;
-
-import java.io.IOException;
+import com.duke.task.Deadline;
 
 /**
  * Encapsulates a deadline command to create a deadline task.
  */
-public class DeadlineCommand extends Command {
-    private static final String TASK_ADDED_MESSAGE = "Got it. I've added this task:";
-
-    private Storage storage;
+public class DeadlineCommand extends AddCommand {
 
     /**
      * Constructs a DeadlineCommand object.
@@ -23,18 +17,5 @@ public class DeadlineCommand extends Command {
     public DeadlineCommand(TaskList list, String input) {
         super(list);
         super.task = Deadline.genDeadline(input);
-        this.storage = new Storage(FILE_PATH);
-    }
-
-    /**
-     * Executes the command.
-     *
-     * @return Output shown to user.
-     * @throws IOException If file is not found or cannot be read.
-     */
-    public String execute() throws IOException {
-        super.list.addTask(super.task);
-        this.storage.appendText(super.task.toString());
-        return stringifyTask(super.list, super.task, TASK_ADDED_MESSAGE);
     }
 }
